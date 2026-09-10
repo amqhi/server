d1ddc8b76ff972226fe222adf97906512b091353
siar1234 <siar019283@gmail.com>
2026-09-10 10:26:19 +0900

    fix: Save sync event on folder creation

--- src/main/kotlin/routes/folders.kt
+++ src/main/kotlin/routes/folders.kt
@@ -8,19 +8,30 @@ package com.amqhi.routes
 
 import com.amqhi.common.withAuth
 import com.amqhi.models.ItemAttributes
+import com.amqhi.models.ItemType
+import com.amqhi.models.SyncEventType
 import com.amqhi.services.AuthService
 import com.amqhi.services.FoldersService
+import com.amqhi.services.SyncEventsService
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.ext.web.Router
 
-fun Router.mountFoldersRouter(authService: AuthService, foldersService: FoldersService) {
+fun Router.mountFoldersRouter(authService: AuthService, foldersService: FoldersService, syncEventsService: SyncEventsService) {
     post("/folders").handler { context ->
         context.withAuth(authService) { user ->
             val body = context.body().asJsonObject()
             foldersService.createFolder(
                 itemAttributes = ItemAttributes.from(user.id, body)
             ).onSuccess { folder ->
-                context.response().setStatusCode(HttpResponseStatus.CREATED.code()).end(folder.toSummaryJson().toString())
+                syncEventsService.createEvent(
+                    itemId = folder.id,
+                    eventType = SyncEventType.CREATE,
+                    bitMask = user.deviceBitMask,
+                    userId = user.id,
+                    itemType = ItemType.FOLDER
+                ).onComplete {
+                    context.response().setStatusCode(HttpResponseStatus.CREATED.code()).end(folder.toSummaryJson().toString())
+                }
             }
                 .onFailure {
                     // TODO: Implement onFailure block for POST /folders
